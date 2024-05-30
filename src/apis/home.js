1bc84a177050e5090318d0a2da8bf5074e1b4bc2
wtfsb19@gmail.com <wtfsb19@gmail.com>
2024-05-30 14:40:12 +0800

    Home -- 新鲜好物和人气推荐完成

--- src/apis/home.js
+++ src/apis/home.js
@@ -2,8 +2,24 @@ import httpInstance from "@/utils/http.js";
 
 
 // 获取banner图片列表
-export function getBannerAPI (){
-  return httpInstance({
-    url:'home/banner'
-  })
+export function getBannerAPI() {
+    return httpInstance({
+        url: 'home/banner'
+    })
+}
+
+
+// 获取新品数据
+export function getNewAPI() {
+    return httpInstance({
+        url: '/home/new'
+    })
+}
+
+
+// 获取人气推荐商品
+export function getHotAPI() {
+    return httpInstance({
+        url:'home/hot'
+    })
 }
