1ed2e38f3de270f84e85eeecd568b60a42f58a0e
shaobing <wtfsb19@gmail.com>
2024-06-04 15:03:56 +0800

    本地购物车 -- 购物车列表模板和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,52 +1,58 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {createRouter, createWebHistory} from 'vue-router'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import Login from '@/views/Login/index.vue'
-
+import CartList from "@/views/Cart/CartList.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'layout',
-      component: Layout,
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: '',
-          name: 'home',
-          component: Home
+            path: '/',
+            name: 'layout',
+            component: Layout,
+            children: [
+                {
+                    path: '',
+                    name: 'home',
+                    component: Home
+                },
+                {
+                    path: '/category/:id',  // params参数
+                    name: 'category',
+                    component: Category
+                },
+                {
+                    path: 'sub/category/:id',  // params参数
+                    name: 'subCategory',
+                    component: SubCategory
+                },
+                {
+                    path: 'detail/:id',
+                    name: 'detail',
+                    component: Detail
+                },
+                {
+                    path: 'cart/list',
+                    name: 'cartList',
+                    component: CartList,
+                }
+            ]
         },
         {
-          path: '/category/:id',  // params参数
-          name: 'category',
-          component: Category
+            path: '/login',
+            name: 'login',
+            component: Login
         },
-        {
-          path: 'sub/category/:id',  // params参数
-          name: 'subCategory',
-          component: SubCategory
-        },
-        {
-          path: 'detail/:id',
-          name: 'detail',
-          component: Detail
-        }
-      ]
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-  ],
-  // 滚动行为，回到顶部
-  scrollBehavior() {
-    return { top: 0 }
-  }
+
+    ],
+    // 滚动行为，回到顶部
+    scrollBehavior() {
+        return {top: 0}
+    }
 })
 
 export default router
