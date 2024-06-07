a089f1358aca5f18c9f44a03b4baf48d49dcb9d6
shaobing <wtfsb19@gmail.com>
2024-06-07 15:20:19 +0800

    会员中心 -- 模板准备和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,10 @@ import CartList from "@/views/Cart/CartList.vue";
 import Checkout from "@/views/Checkout/index.vue";
 import Pay from "@/views/Pay/index.vue";
 import PayBack from "@/views/Pay/components/PayBack.vue";
+import Member from "@/views/Member/index.vue";
+import UserInfo from "@/views/Member/components/UserInfo.vue";
+import UserOrder from "@/views/Member/components/UserOrder.vue";
+
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -58,6 +62,24 @@ const router = createRouter({
                     path: 'paycallback',
                     name: 'paycallback',
                     component: PayBack
+                },
+                {
+                    path: 'member',
+                    name: 'member',
+                    component: Member,
+                    children: [
+                        {
+                            path: '',
+                            name: 'userInfo',
+                            component: UserInfo
+                        },
+                        {
+                            path: 'order',
+                            name: 'userOrder',
+                            component: UserOrder
+                        }
+                    ]
+
                 }
             ]
         },
